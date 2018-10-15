054883c698247fbe902b35a93b689d2ec9bfe58d
fly-man- <fly.man.opensim@gmail.com>
2018-10-15 23:43:46 +0200

    Switch the files out

---
+++ max.php
@@ -0,0 +1,27 @@
+<?php
+$id = $_GET['t'];
+switch($id)
+{
+    case 0:
+        echo '1002';//fur
+        break;
+    case 1:
+        echo '1004';//ear
+        break;
+    case 2:
+        echo '1008';//eye
+        break;
+    case 3:
+        echo '1004';//tail
+        break;
+    case 4:
+        echo '1007';//shade
+        break;
+    case 5:
+        echo '1001';//luster
+        break;
+    case 6:
+        echo '1002';//gleam
+        break;
+}
+?>
